c50ad7425d6510df1bb9eb0cdf41d21dae04ca57
kjm4311 <moon4311@gmail.com>
2023-01-01 19:58:48 +0900

    메뉴 동적으로 변경

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+naive-ui
+
+
 # V-Dashboard
 
 Dashboard starter template built with Vite, Vue 3, Tailwind CSS and TypeScript.
--- src/router.ts
+++ src/router.ts
@@ -1,138 +1,54 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import _ from 'lodash'
 
 // Sample
-import Dashboard from "./views/sample/Dashboard.vue";
-import Forms from "./views/sample/Forms.vue";
-import Tables from "./views/sample/Tables.vue";
-import UIElements from "./views/sample/UIElements.vue";
-import Login from "./views/sample/Login.vue";
-import Modal from "./views/sample/Modal.vue";
-import Card from "./views/sample/Card.vue";
-import Blank from "./views/sample/Blank.vue";
-import NotFound from "./views/NotFound.vue";
+import Login from "./views/Login.vue";
 
-import EmptyLayout from "./layout/EmptyLayout.vue";
+import axios from 'axios';
+import apiUrl from './assets/base';
 
 
-
-
-// import User from ;
-import UserInfo from "./views/admin/cmm/UserInfo.vue";
-import Menu from "./views/admin/cmm/Menu.vue";
-import CodeList from "./views/admin/cmm/CodeList.vue";
-import Board from "./views/admin/cmm/Board.vue";
-import AuthName from "./views/admin/cmm/AuthName.vue";
-import AuthRole from "./views/admin/cmm/AuthRole.vue";
-import AuthGrant from "./views/admin/cmm/AuthGrant.vue";
-
-
-const routes: RouteRecordRaw[] = [
-  {
-    path: "/",
-    name: "Login",
-    component: Login,
-    meta: { layout: "empty" },
-  },
-  {
-    path: "/admin",
-    name: "사용자",
-    component: import("./layout/EmptyLayout.vue"),
-    children:[
-      {
-        path:"user",
-        name : "사용자관리",
-        component: import("./views/admin/cmm/User.vue")
-      },
-      {
-        path:"user/:id",
-        name : "사용자 상세",
-        component:()=> import("./views/admin/cmm/UserInfo.vue")
-      }
-    ]
-  },
-  
-  {
-    path: "/admin/auth",
-    name: "권한",
-    component: EmptyLayout,
-    children:[
-          {
-            path: "authName",
-            name: "권한명관리",
-            component: AuthName,
-          },
-          {
-            path: "authRole",
-            name: "권한롤관리",
-            component: AuthRole,
-          },
-          {
-            path: "authGrant",
-            name: "권한부여",
-            component: AuthGrant,
-          }
-        ]
-  },
-  {
-    path: "/admin/system",
-    name: "시스템",
-    component: EmptyLayout,
-    children:[
-      {
-        path: "code",
-        name: "공통코드관리",
-        component: CodeList,
-      },
-      {
-        path: "menu",
-        name: "메뉴관리",
-        component: Menu,
-      },
-    ]
-  },
-  {
-    path: "/admin/board",
-    name: "게시판관리",
-    component: Board,
-  },
-
-  {
-    path: "/sample/dashboard",
-    name: "Dashboard",
-    component: Dashboard,
-  },
-  {
-    path: "/sample/forms",
-    name: "Forms",
-    component: Forms,
-  },
-  {
-    path: "/sample/cards",
-    name: "Cards",
-    component: Card,
-  },
-  {
-    path: "/sample/tables",
-    name: "Tables",
-    component: Tables,
-  },
-  {
-    path: "/sample/ui-elements",
-    name: "UIElements",
-    component: UIElements,
-  },
-  {
-    path: "/sample/modal",
-    name: "Modal",
-    component: Modal,
-  },
-  {
-    path: "/sample/blank",
-    name: "Blank",
-    component: Blank,
-  },
+let routes: any[] = [{
+  path: "/",
+  name: "Login",
+  component: Login,
+  meta: { layout: "empty" },
+},
 ];
 
+
+function getMenu(b: any){
+  var menu = { path : b.path, name : b.name, component : import("./layout/EmptyLayout.vue") };
+  if(b.component!=null) menu.component = import( "./views/" + b.component + ".vue") 
+  // if(b.component!=null) menu.component = () => import( "./views/" + b.component + ".vue") 
+  return menu;
+}
+
+await axios.get(apiUrl+"/menu/list").then((res)=>{
+  var list = res.data.data;
+  //childen 생성
+  list.forEach((a)=> 
+    {
+      a.children = [];
+      _.forEach(list,(b,idx)=>{
+        if(a.menuId == b.upperMenuId){
+          list[idx] = getMenu(b);
+          a.children.push( list[idx] );
+        }// if
+      });//forEach
+      if(a.children.length==0) delete a.children;
+    });
+  //최상위 메뉴만
+  _.forEach(list,(a)=>{ 
+    if(a.menuId){
+      var menu = getMenu(a);
+      if(a.children) menu.children = a.children;
+      routes.push( menu );
+    }
+  });
+});
+
+console.log(routes);
 const router = createRouter({
   history: createWebHistory(),
   routes: routes,
