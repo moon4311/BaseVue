97cb0d87b5fd7bcc5f43de1feba0f2a904d2a1b2
kimjaemoon <moon4311@gmail.com>
2023-01-13 09:44:56 +0900

    main.ts
    main.ts 변경

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import router from './router';
 import naive from 'naive-ui';
 import './assets/main.css';
 
+import _ from 'lodash';
+
+
+
 import DashboardLayout from './layout/DashboardLayout.vue';
 import SampleLayout from './layout/SampleLayout.vue';
 import EmptyLayout from './layout/EmptyLayout.vue';
@@ -16,4 +20,11 @@ app.component('empty-layout', EmptyLayout);
 
 app.use(router);
 app.use(naive);
+
+app.config.globalProperties.$axios = axios;
+app.config.globalProperties._ = _;
+
+app.provide('$axios',axios);
+app.provide('_',_);
+
 app.mount('#app');
