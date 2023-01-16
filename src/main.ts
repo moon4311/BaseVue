499736fa3f75a653b39c66c19c54d84b7682bd52
kjm4311 <moon4311@gmail.com>
2023-01-17 01:20:21 +0900

    디자인 일부 수정

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import naive from 'naive-ui';
+import axios from 'axios';
 import './assets/main.css';
 
 import _ from 'lodash';
