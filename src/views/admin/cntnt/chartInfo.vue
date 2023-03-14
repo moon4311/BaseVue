3e71a348a069e9cc2eeaef8cb458d8a1afb5aa36
jaeMoon <moon4311@gmail.com>
2023-03-14 12:53:49 +0900

    chartInfo 수정

--- src/views/admin/cntnt/chartInfo.vue
+++ src/views/admin/cntnt/chartInfo.vue
@@ -6,17 +6,25 @@
   <n-card>
     <div>
     <div class="mt-8">
-      <n-form-item path="title" label="제목">
-        <n-input v-model:value="params.title" />
+      <n-form-item path="chartTypeCd" label="구분">
+        <!-- TODO. select box -->
+        <n-select :options="options" />
+        <n-input v-model:value="params.chartTypeCd" />
       </n-form-item>
-      <n-form-item path="name" label="이름">
-        <n-input v-model:value="params.name"/>
+      <n-form-item path="chartNm" label="이름">
+        <n-input v-model:value="params.chartNm"/>
       </n-form-item>
-      <n-form-item path="contents" label="내용">
-        <n-input v-model:value="params.contents"/>
+      <n-form-item path="width" label="넓이">
+        <n-input v-model:value="params.width"/>
       </n-form-item>
-      <n-form-item path="memo" label="메모">
-        <n-input v-model:value="params.memo"/>
+      <n-form-item path="height" label="높이">
+        <n-input v-model:value="params.height"/>
+      </n-form-item>
+      <n-form-item path="labels" label="라벨">
+        <n-input v-model:value="params.labels"/>
+      </n-form-item>
+      <n-form-item path="datasets" label="데이터셋">
+        <n-input v-model:value="params.datasets"/>
       </n-form-item>
       <n-row :gutter="[0, 24]">
         <n-col :span="24">
@@ -41,11 +49,21 @@ import { useMessage } from 'naive-ui'
   const message = useMessage()
   const id = useRoute().params.id;
   const params = ref({})
+  const options =ref([]);
   const apiPath = 'chart';
   onMounted( ()=>{
+
+    axios.get(apiUrl + "/code/options/chart_type_cd").then(res=>{
+      options.value = res.data.data;
+    });
+
     if(id){
       axios.get(apiUrl + "/"+apiPath+"/info/"+ id ).then(res=>{
         params.value=res.data.data;
+
+        // params.value.labels = JSON.parse(params.value.labels);
+        // params.value.datasets = JSON.parse(params.value.datasets);
+
       })
     }
   });
